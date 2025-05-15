95488347cc98697c08fa51e0aee9eeb76e1f3a29
Dmitri Kuznetsov <kna_int@mail.ru>
2025-05-15 21:34:55 +0300

    Included lib to the project

--- Godot/godotvisualizer/MainScene.cs
+++ Godot/godotvisualizer/MainScene.cs
@@ -1,3 +1,4 @@
+using AuroraLib;
 using Godot;
 using System;
 
@@ -6,7 +7,9 @@ public partial class MainScene : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
+		var cl = new Class1();
+        GD.Print($"Info: MainScene is ready = {cl.GetMessage()}");         // Standard log
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
